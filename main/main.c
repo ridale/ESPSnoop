320d1e66c3453b5c6488c32a1a2e48959e161444
Richard Lemon <richard@codelemon.com>
2019-09-24 17:15:51 +1000

    initial code

---
+++ main/main.c
@@ -0,0 +1,14 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+extern void bt_task(void *ignore);
+
+void init_scan(void)
+{
+
+}
+
+void app_main(void)
+{
+  xTaskCreatePinnedToCore(&bt_task, "btTask", 2048, NULL, 5, NULL, 0);
+}
